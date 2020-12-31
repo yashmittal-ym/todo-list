213cee97b883a23703a8f88f12adc9171c26b992
Yash Mittal <yash.mittal7417@gmail.com>
2020-12-31 13:52:47 +0530

    simple react based static todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import ToDoList from './ToDoList'
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+  const ListOfItems = () => {
+    setItems((oldItem) => {
+      return [...oldItem, inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteItems = (id) => {
+    setItems((oldItem) => {
+      return oldItem.filter((arrElement, index) => {
+        return index !== id;
+      });
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="centre_div">
+          <br />
+          <h1>To Do List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add an item"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={ListOfItems}>+</button>
+          <ol>
+            {
+              Items.map((itemVal, index) => {
+                //if string is size we won't add anything
+                if (itemVal.length > 0) {
+                  return <ToDoList
+                    key={index}
+                    id={index}
+                    text={itemVal}
+                    del={deleteItems} />;
+                }
+              })
+            }
+          </ol>
+        </div>
+      </div>
+    </>
   );
-}
-
+};
 export default App;
